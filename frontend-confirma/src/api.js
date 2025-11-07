6ffa74c0d1b09b6f70daf1dba2358d1c9aaed7e7
AndresBerdu <andres700crac@gmail.com>
2025-11-06 21:23:54 -0500

    urls cambiadas

--- frontend-confirma/src/api.js
+++ frontend-confirma/src/api.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://purple-river-041ce360f.3.azurestaticapps.net'; //'http://localhost:4000/api'; //'https://eventoapp.onrender.com/api';
+export const API_URL = 'https://backendevento-cvgtc2akeye8dzf2.westus3-01.azurewebsites.net/api'; //'http://localhost:4000/api'; //'https://eventoapp.onrender.com/api';
 /* Forzar */
